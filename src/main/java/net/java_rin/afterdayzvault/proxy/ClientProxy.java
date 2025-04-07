67171ff5fbf2bfd2f3d9747c700fb50c4a6141ae
rinx <duggerseven@gmail.com>
2025-04-07 15:51:23 +0700

    rename CommonProxy to IProxy

--- src/main/java/net/java_rin/afterdayzvault/proxy/ClientProxy.java
+++ src/main/java/net/java_rin/afterdayzvault/proxy/ClientProxy.java
@@ -4,7 +4,7 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 
-public class ClientProxy extends CommonProxy {
+public class ClientProxy extends IProxy {
 
     @Override
     public void registerItemRenderer(Item item, int meta, String id) {
--- src/main/java/net/java_rin/afterdayzvault/proxy/IProxy.java
+++ src/main/java/net/java_rin/afterdayzvault/proxy/IProxy.java
@@ -2,7 +2,7 @@ package net.java_rin.afterdayzvault.proxy;
 
 import net.minecraft.item.Item;
 
-public class CommonProxy {
+public class IProxy {
     public void registerItemRenderer(Item item, int meta, String id) {}
 
 }
